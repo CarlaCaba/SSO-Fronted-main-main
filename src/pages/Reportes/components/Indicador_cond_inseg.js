358c42630d1868f1a0a9430cea33bee07d8a05e1
dennisisrl <dipa27.2009@gmail.com>
2022-10-04 12:19:47 -0500

    modificado

--- src/pages/Reportes/components/Indicador_cond_inseg.js
+++ src/pages/Reportes/components/Indicador_cond_inseg.js
@@ -2,7 +2,6 @@ import React from 'react';
 import '../../Reportes/Reportes.css'
 import estadistica from '../../Reportes/components/estadistica.png'
 import logo from '../../Reportes/components/logo_login.png'
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -33,6 +32,72 @@ const Indicador_cond_inseg = () => {
     
         <br></br>
         <div className='indicInseg'> <h3>Indicador de Condiciones Inseguras</h3></div>
+       
+        <table className=' table-bordered ' >   
+          <thead className=' table-active '>
+  
+            <tr>
+           
+              <th> Indicador N°19</th>  
+             <th>1 1. INFORMACIÓN DEL INDICADOR </th> 
+             <th></th>
+             <th></th>
+            </tr>
+           
+          </thead>
+          
+          <tbody>
+            
+            <tr>
+              <th>Nombre Indicador </th>
+              <td>Tablero de indicadores </td>
+              <th>Codigo: </th>
+              <td> DI-TH-SO-C-008</td>
+            </tr>
+            <tr>
+              <th>Finalidad</th>
+              <td>Eficiencia personal, grupal y organizacional</td>
+              <th></th>
+              <th></th>
+             
+            </tr>
+            <tr>
+              <th>Calculo</th>
+              <td>(N° cap. seguridad y S. Ocupacional realizadas/ N° cap. programadas)*100 </td>
+              <th></th>
+              <th></th>
+             
+            </tr>
+            <tr>
+              <th>Unidad de medida</th>
+              <td>Porcentaje </td>
+              <td>Frecuencia</td>
+              <td>Mensual</td>
+            </tr>
+            <tr>
+              <th>Meta/ Umbral</th>
+              <th>No existe cumplimiento</th>
+              <th>Cumplimiento regular</th>
+              <th>Cumplimiento eficiente</th>
+            </tr>
+            <tr>
+              <td></td>
+              <td>del 100% al 65%</td>
+              <td>del 64% al 30%</td>
+              <td> menor a 30% </td>
+            </tr>
+            
+
+          </tbody>
+          </table >
+
+        
+      
+
+        
+
+      
+
         <br/>
         <div className='table-resposive'>
           <table className='table table-bordered' padding="2px">
